d73d75c34c610171b83ae6cadd1138a1dd8aba06
Paola_G <perfectblue@live.com>
2024-03-30 14:04:35 -0700

    added navLink.jsx file that will highlight page link that user is currently on, implemented NavLink Component to Navbar component, and fixed bugs in styling of links, social media links, and logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
+import NavLink from "./navLink";
 
 const links = [
   { url: "/", title: "Home" },
@@ -14,17 +15,15 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
-      <div className="hidden md:flex gap-4 xl:w-1/3">
+      <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex xl:w-1/3 justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -36,7 +35,7 @@ const Navbar = () => {
         </Link>
       </div>
       {/* SOCIAL MEDIA */}
-      <div className="hidden md:flex gap-4 xl:w-1/3">
+      <div className="hidden md:flex gap-4 w-1/3">
         <Link href="https://github.com/perfectblue0">
           <Image
             src="/github.png"
