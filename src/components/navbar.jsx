d9222ac68314dc00a4f9dd2454babb69b5f250b7
Paola_G <perfectblue@live.com>
2024-03-30 13:35:37 -0700

    added navigation links, social media images with links to my profiles, and responsive stack menu icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,81 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import Image from "next/image";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      {/* LOCO */}
-      <div className="">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 xl:w-1/3">
+        {links.map((link) => (
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      {/* LOGO */}
+      <div className="md:hidden lg:flex xl:w-1/3 justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
         >
           <span className="text-white mr-1">Avid</span>
-          <span className="w-8 h-8 rounded bg-white text-black flex items-center justify-center">
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
             Dev
           </span>
         </Link>
       </div>
+      {/* SOCIAL MEDIA */}
+      <div className="hidden md:flex gap-4 xl:w-1/3">
+        <Link href="https://github.com/perfectblue0">
+          <Image
+            src="/github.png"
+            alt="github logo with link to my profile"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link href="https://www.linkedin.com/in/paola-g-lupercio/">
+          <Image
+            src="/linkedin.png"
+            alt="linkedIn logo with link to my profile"
+            width={24}
+            height={24}
+          />
+        </Link>
+      </div>
       {/* RESPONSIVE MENU */}
-      <div>2</div>
+      <div className="md:hidden">
+        {/* MENU BUTTON */}
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+        </button>
+        {/* MENU LIST */}
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
