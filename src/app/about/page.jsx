90baf33e545bac071a7560f1241b3c12d0ab5abc
Paola_G <perfectblue@live.com>
2024-04-10 16:14:13 -0700

    added skills with styled buttons with effects to about page, started to add experience as well

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -12,13 +12,13 @@ const AboutPage = () => {
       {/* CONTAINER */}
       <div className="">
         {/* TEXT CONTAINER */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-49 xl:gap-64">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
           {/* BIOGRAPHY CONTAINER */}
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY TITLE */}
             <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
             {/* BIOGRAPHY DESCRIPTION */}
-            <p className="text-xl">
+            <p className="text-lg">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
               nec, ultricies sed, dolor. Cras elementum ultrices diam.
@@ -64,9 +64,156 @@ const AboutPage = () => {
             </svg>
           </div>
           {/* SKILLS CONTAINER */}
-          <div className="">SKILLS</div>
+          <div className="flex flex-col gap-12 justify-center">
+            {/* SKILL TITLE */}
+            <h1 className="font-bold text-2xl">SKILLS</h1>
+            {/* SKILL LIST */}
+            <div className="flex gap-4 flex-wrap">
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                JavaScript
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                HTML
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                CSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Bootstrap
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Tailwind CSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Framer Motion
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                React.js
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Next.js
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Node.js
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Express.js
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                MySQL
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                MongoDB
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Git
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                GitHub
+              </div>
+            </div>
+            {/* SKILL SCROLL SVG */}
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </svg>
+          </div>
           {/* EXPERIENCE CONTAINER */}
-          <div className="">EXPERIENCE</div>
+          <div className="flex flex-col gap-12 justify-center pb-48">
+            <h1 className="font-bold text.2xl">EXPERIENCE</h1>
+            {/* EXPERIENCE LIST */}
+            <div className="">
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="">
+                {/* left */}
+                <div className=""></div>
+                {/* center */}
+                <div className="">
+                  {/* LINE */}
+                  <div className="">
+                    {/* LINE CIRCLE */}
+                    <div className=""></div>
+                  </div>
+                </div>
+                {/* right */}
+                <div className="">
+                  {/* CERTIFICATE TITLE */}
+                  <div className="">Certificate Title</div>
+                  {/* CERTIFICATE DESCRIPTION */}
+                  <div className="">a brief description of the certificate</div>
+                  {/* CERTIFICATE DATE */}
+                  <div className="">August 2024</div>
+                  {/* WHERE CERTIFICATE IS FROM */}
+                  <div className="">Company</div>
+                </div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* left */}
+                <div className="w-1/3">
+                  {/* CERTIFICATE TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Certificate Title
+                  </div>
+                  {/* CERTIFICATE DESCRIPTION */}
+                  <div className="">a brief description of the certificate</div>
+                  {/* CERTIFICATE DATE */}
+                  <div className="">August 2024</div>
+                  {/* WHERE CERTIFICATE IS FROM */}
+                  <div className="">Company</div>
+                </div>
+                {/* center */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="">
+                    {/* LINE CIRCLE */}
+                    <div className=""></div>
+                  </div>
+                </div>
+                {/* right */}
+                <div className="w-1/3"></div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="">
+                {/* left */}
+                <div className="">
+                  {/* CERTIFICATE TITLE */}
+                  <div className="">Certificate Title</div>
+                  {/* CERTIFICATE DESCRIPTION */}
+                  <div className="">a brief description of the certificate</div>
+                  {/* CERTIFICATE DATE */}
+                  <div className="">August 2024</div>
+                  {/* WHERE CERTIFICATE IS FROM */}
+                  <div className="">Company</div>
+                </div>
+                {/* center */}
+                <div className="">
+                  {/* LINE */}
+                  <div className="">
+                    {/* LINE CIRCLE */}
+                    <div className=""></div>
+                  </div>
+                </div>
+                {/* right */}
+                <div className=""></div>
+              </div>
+            </div>
+          </div>
         </div>
         {/* SVG CONTAINER */}
         <div className="hidden"></div>
